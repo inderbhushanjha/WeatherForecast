740c0133c6690a533d4d9ffb207af42384e25c25
inderbhushanjha <i.jha32@gmail.com>
2021-05-28 03:36:08 +0530

    done setting up login signup and logout functionality working on weather api

--- cityforecast/urls.py
+++ cityforecast/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup',views.signup,name='signup'),
     path('home',views.home,name='signup'),
     path('about',views.about,name='signup'),
+    path('logout', views.logout, name='logout')
 ]
--- cityforecast/views.py
+++ cityforecast/views.py
@@ -1,28 +1,58 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from decouple import config
 # Create your views here.
 
 
 def index(request):
+    API_KEY = config('WEATHER_API')
+    print(type(API_KEY))
+    print(API_KEY)
+    st = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid='
     return render(request,'index.html')
 
 def login(request):
     if request.method == 'POST':
-        uname = request.POST['username']
+        username = request.POST['username']
         password = request.POST['password']
-        print(uname)
-        print(password)
+        user = auth.authenticate(username = username, password = password)
+        if user is not None:
+            auth.login(request,user)
+            return HttpResponseRedirect('home')
+        else:
+            messages.info(request, 'Incorrect Username or Password')
+            return HttpResponseRedirect('login')
+
     return render(request,'login.html')
+
+# logout function
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect('/')
+
 
 def signup(request):
     if request.method == 'POST':
-        fname = request.POST['fullname']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
         email = request.POST['email']
         username = request.POST['username']
-        p1 = request.POST['password1']
-        p2 = request.POST['password2']
-        tandc = request.POST['tc']
-        
+        password = request.POST['password']
+
+        if User.objects.filter(username = username).exists():  #check user already exists in the database.
+            messages.info(request, 'Username Already Taken!')
+            return HttpResponseRedirect('signup')
+        if User.objects.filter(email = email).exists():
+            messages.info(request, 'Email Already Taken!')
+            return HttpResponseRedirect('signup')
+        else:
+            user = User.objects.create_user(username = username, password = password, email = email,first_name = first_name, last_name = last_name)
+            user.save()
+            return HttpResponseRedirect('login')
+
     return render(request, 'signup.html')
 
 def home(request):
